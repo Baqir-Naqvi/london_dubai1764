1e8a659ea034fd79cdca95c60a35c3df89d68f92
Hanan Munir <hananmunir471gmail.com>
2023-07-15 23:01:01 +0500

    added images

---
+++ components/Animation.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { gsap, SteppedEase } from "gsap";
+
+import TypeIt from "typeit-react";
+
+export default function Animation() {
+  const textRef = useRef(null);
+  useEffect(() => {
+    if (textRef.current) {
+      var tl = new gsap.timeline({
+        paused: true,
+      });
+      tl.to(textRef.current, {
+        y: -600,
+        duration: 1,
+
+        delay: 0.5,
+      });
+      tl.play();
+    }
+  }, [textRef.current]);
+  return (
+    <div textRef={textRef} className=' border text-9xl line-1 anim-typewriter'>
+      hey
+      {/* <TypeIt>2023 </TypeIt> */}
+    </div>
+  );
+}
