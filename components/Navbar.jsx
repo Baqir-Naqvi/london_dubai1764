9696e38817dc500c340027aa41bb9e5be8f8b7a9
Baqir Naqvi <b.naqvi99@gmail.com>
2023-07-21 20:02:57 +0500

    Navbar Links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
       role="navigation"
     >
         <ul className="md:flex flex-row justify-center items-center md:h-16 text-white  font-mono w-full h-0 overflow-hidden">
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">ABOUT</li>
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">OUT STORY</li>
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">MEMBERSHIP</li>
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">GLOBAL CLUBS</li>
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">FAQ</li>
-            <li className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">FOUNDERS</li>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">ABOUT</a>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">OUR STORY</a>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">MEMBERSHIP</a>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">GLOBAL CLUBS</a>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">FAQ</a>
+            <a className="hover:text-gray-300 hover:font-bold hover:cursor-pointer p-4">FOUNDERS</a>
           
         </ul>
 
